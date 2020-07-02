cb3ff33c2a3ee83c8026a4dcc2d63fdf891e8de7
oyvindsol <oyvindsolberg.pro@gmail.com>
2020-07-02 13:13:45 +0200

    Fixed correct API URL

--- client/src/axios-PortfolioAPI.js
+++ client/src/axios-PortfolioAPI.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const instance = axios.create({
   // Development URL
-  baseURL: 'http://localhost:8000/api/v1',
+  // baseURL: 'http://localhost:8000/api/v1',
 
   // Production URL
-  // baseURL: 'https://portfolio-oyvind-solberg.herokuapp.com/api/v1',
+  baseURL: 'https://portfolio-oyvind-solberg.herokuapp.com/api/v1',
 });
 
 export default instance;
