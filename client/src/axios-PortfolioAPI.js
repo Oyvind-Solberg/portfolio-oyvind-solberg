f049d90db4e9f3af4ac7046a96c39e2aed41f518
oyvindsol <oyvindsolberg.pro@gmail.com>
2020-11-27 13:21:12 +0100

    Change dataBar size on mobile

--- client/src/axios-PortfolioAPI.js
+++ client/src/axios-PortfolioAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // Development URL
-  // baseURL: 'http://localhost:8000/api/v1',
+  // baseURL: 'http://10.2.0.16:8000/api/v1',
 
   // Production URL
   baseURL: 'https://portfolio-oyvind-solberg.herokuapp.com/api/v1',
